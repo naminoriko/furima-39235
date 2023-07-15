555a8a7c5a78eac94bed40a0a0aa37934e6e7915
naminoriko <dankan1003@gmail.com>
2023-07-15 15:22:21 +0900

    エラーメッセージ表示と新規登録のモデルにバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-
+  validates :nickname,        presence: true
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name_kana,  presence: true, format: { with: /\p{katakana}/ }
+  validates :first_name_kana, presence: true, format: { with: /\p{katakana}/ }
+  validates :birthday,        presence: true
+  
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
 
