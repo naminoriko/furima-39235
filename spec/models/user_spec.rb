58e63d1f1a1f5e61429394c4230f0d4c47719d5a
naminoriko <dankan1003@gmail.com>
2023-07-15 20:16:11 +0900

    テストコードnicknameが空では登録できない

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      user = User.new(nickname: '', email: 'test@example', password: '000abc', password_confirmation: '000abc', last_name: '鈴木', first_name: '一郎', last_name_kana: 'スズキ', first_name_kana: 'イチロウ', birthday: '1980-01-01' )
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it 'last_nameが空では登録できない' do
+    end
+    it 'first_nameが空では登録できない' do
+    end
+    it 'last_name_kanaが空では登録できない' do
+    end
+    it 'first_name_kanaが空では登録できない' do
+    end
+    it 'birthdayが空ではと黒くできない' do
+    end
+
+  end
 end
