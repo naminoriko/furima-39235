47e405e62ce8f4a56c07ee90189ce07d0d678189
naminoriko <dankan1003@gmail.com>
2023-07-15 20:54:22 +0900

    Factory_botとFakerの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
-      user = User.new(nickname: '', email: 'test@example', password: '000abc', password_confirmation: '000abc', last_name: '鈴木', first_name: '一郎', last_name_kana: 'スズキ', first_name_kana: 'イチロウ', birthday: '1980-01-01' )
-      user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it 'emailが空では登録できない' do
+    end
+    it 'passwordが空では登録できない' do
+    end
+    it 'password_confirmationが空では登録できない' do
     end
     it 'last_nameが空では登録できない' do
     end
