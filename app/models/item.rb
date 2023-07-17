307ad0b923cd41bef992822d4000e93dd3b2269f
naminoriko <dankan1003@gmail.com>
2023-07-17 20:29:29 +0900

    各モデルにActiveHash::Baseを継承、データを定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_payment
+  belongs_to :sender_area
+  belongs_to :number_of_day
 end
