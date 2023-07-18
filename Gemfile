8d7985bd8bd391554813217f588a87d9fdf82f75
naminoriko <dankan1003@gmail.com>
2023-07-18 22:09:51 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -65,7 +65,7 @@ group :production do
 end
 
 gem 'devise'
-
 gem 'pry-rails'
-
 gem 'active_hash'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,13 @@ class ItemsController < ApplicationController
 def index
 end
 
+private
+
+def item_params
+  params.require(:item).permit(
+    :image, :name, :content, :category_id, :condition_id, :delivery_id, 
+    :sender_area_id, :number_of_day_id, :price
+    ).merge(user_id: current_user.id)
+end
+
 end
