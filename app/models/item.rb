6535fa4f20e43f501f3ede445231ba749151ef54
naminoriko <dankan1003@gmail.com>
2023-07-18 21:42:10 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,9 @@ class Item < ApplicationRecord
   belongs_to :delivery_payment
   belongs_to :sender_area
   belongs_to :number_of_day
+
+  validates :name, :content, :price, presence: true
+  validates :category_id, :condition_id, :delivery_payment_id, 
+            :sender_area_id, :number_of_day_id, 
+            numericality: { other_than: 1 , message: "can't be blank" } 
 end
