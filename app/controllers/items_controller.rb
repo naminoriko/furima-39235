5192b77728d56aef02a9094e7dbc0db6cd5322ae
naminoriko <dankan1003@gmail.com>
2023-07-23 18:22:11 +0900

    privateメソッド内の記述を編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ private
 
 def item_params
   params.require(:item).permit(
-    :image, :name, :content, :category_id, :condition_id, :delivery_id, 
+    :image, :name, :content, :category_id, :condition_id, :delivery_payment_id, 
     :sender_area_id, :number_of_day_id, :price
     ).merge(user_id: current_user.id)
 end
