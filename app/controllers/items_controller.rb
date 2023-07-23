a588f88c29b31060c9c84c6b5f42654446cee7ec
naminoriko <dankan1003@gmail.com>
2023-07-23 18:19:09 +0900

    newとcreateアクションを編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,16 @@ def index
 end
 
 def new
+  @item = Item.new
+end
+
+def create
+  @item = Item.new(item_params)
+  if @item.save
+    redirect_to root_path
+  else
+    render :new
+  end
 end
 
 private
