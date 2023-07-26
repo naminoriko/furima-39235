9f05c940be64fdfcf4dd38159636c483da7a7c80
naminoriko <dankan1003@gmail.com>
2023-07-26 21:01:00 +0900

    FactoryBotの記述を修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,15 +1,18 @@
 FactoryBot.define do
   factory :item do
-    image               {Faker::Lorem.sentence}
-    name                {Faker::Name}
+    name                {Faker::Lorem.sentence}
     content             {Faker::Lorem.sentence}
-    category_id         { '1' }
-    condition_id        { '1' }
-    delivery_payment_id { '1' }
-    sender_area_id      { '1' }
-    number_of_day_id    { '1' }
+    category_id         { 2 }
+    condition_id        { 2 }
+    delivery_payment_id { 2 }
+    sender_area_id      { 2 }
+    number_of_day_id    { 2 }
     price               {Faker::Number.between(from: 300, to: 9999999)}
     association :user 
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
 
