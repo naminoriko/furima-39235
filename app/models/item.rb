76d047e81eb2b6f096bfe5f62a36d85b0943e503
naminoriko <dankan1003@gmail.com>
2023-07-29 10:03:57 +0900

    priceバリデーションを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   validates :category_id, :condition_id, :delivery_payment_id,
             :sender_area_id, :number_of_day_id,
             numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { greater_than: 300, less_than: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
 
   belongs_to :user
   has_one    :order
