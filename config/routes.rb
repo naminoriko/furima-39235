10f8a8ec5622f321da162939b96e1df721558ba9
naminoriko <dankan1003@gmail.com>
2023-08-12 16:44:30 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only:[:new, :create, :index, :show, :edit, :update, :destroy]  
+  resources :items, only:[:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :orders, only:[:index]  
   end
