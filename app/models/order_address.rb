aa38e0e759f029616f7046bdd953e2825f87973c
naminoriko <dankan1003@gmail.com>
2023-08-12 17:44:05 +0900

    Update order_address.rb

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,5 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :
+  attr_accessor :post_code, :sender_area_id, :city, :banchi, :building, :phone_number, :order_id,
+                :user_id, :item_id
 end
