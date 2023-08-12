42b2ec712a002776ff5d5f5ee1953dd6db0bc05c
naminoriko <dankan1003@gmail.com>
2023-08-12 18:20:15 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,24 @@
 class OrdersController < ApplicationController
-  
+
   def index
   end
 
+  def new
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :sender_area_id, :city, :banchi, :building, :phone_number, :order_id, :item_id).merge(user_id: current_user.id)
 end
