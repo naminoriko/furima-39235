b6204a93765360b358e97722581019641358d070
naminoriko <dankan1003@gmail.com>
2023-08-14 17:14:49 +0900

    Update order_address.rb

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,7 +5,7 @@ class OrderAddress
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :city, :banchi, :building, :phone_number, :user_id, :item_id
+    validates :city, :banchi, :phone_number, :user_id, :item_id
   end
   validates :sender_area_id, numericality: { other_than: 1, message: "can't be blank" }
 
