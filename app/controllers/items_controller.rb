1da1cd59b750bf524a3d57cba886548251c70eb5
naminoriko <dankan1003@gmail.com>
2023-08-16 08:55:48 +0900

    soldout時の表示を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user == current_user
+    if @item.order.present? == false && @item.user == current_user
       render 'edit'
     else
       redirect_to root_path
